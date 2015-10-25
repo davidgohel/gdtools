6f8dbe0d2d631a56003fa113f08629181a22ea91
David Gohel <david.gohel@ardata.fr>
2015-10-25 08:41:34 +0100

    drop plot raster from example

--- R/raster.R
+++ R/raster.R
@@ -12,7 +12,6 @@
 #' @examples
 #' r <- as.raster(matrix(hcl(0, 80, seq(50, 80, 10)),
 #'  nrow = 4, ncol = 5))
-#' plot(r)
 #' code <- raster_str(r, width = 50, height = 50)
 #' if (interactive() && require("htmltools")) {
 #'   raster_view(code = code)
