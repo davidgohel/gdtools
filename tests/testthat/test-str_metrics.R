ae91c2b5d690bd151a6ea7664d73d2a600b18fac
dgohel <david.gohel@ardata.fr>
2016-10-05 15:26:07 +0200

    minor

--- tests/testthat/test-str_metrics.R
+++ tests/testthat/test-str_metrics.R
@@ -2,25 +2,19 @@ context("font file metric info")
 library(fontquiver)
 
 test_that("a string has positive dimensions", {
-  sans_ <- font_families("Bitstream Vera")$sans
-  value <- str_metrics(x = "Hello World", fontsize = 12, bold = TRUE, italic = TRUE,
-                       fontfile = c(sans_$bold$file) )
+  file <- fontquiver::font_bitstream_vera(style = "bold")$file
+  value <- str_metrics(x = "Hello World", fontsize = 12, fontfile = file )
   expect_true( value["width"] > 0)
   expect_true( value["ascent"] > 0 )
   expect_false( value["descent"] < 0 )
 })
 
 test_that("fallback usage", {
-  sans_ <- font_families("Bitstream Vera")$sans
+  file <- fontquiver::font_bitstream_vera(variant = "sans", style = "bold-oblique")$file
   expect_warning(
-    value_1 <- str_metrics(
-      x = "\u265E", fontsize = 12, bold = FALSE, italic = FALSE,
-      fontfile = sans_$bolditalic$file
-    )
+    value_1 <- str_metrics( x = "\u265E", fontsize = 12, fontfile = file )
   )
-  value_2 <- str_metrics(
-    x = "M", fontsize = 12, bold = FALSE, italic = FALSE,
-    fontfile = sans_$bolditalic$file)
+  value_2 <- str_metrics( x = "M", fontsize = 12, fontfile = file )
   expect_equivalent(value_1, value_2)
 })
 
