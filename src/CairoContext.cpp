d700c0ce007de85e4250a2aa842dd93983ff6f62
Lionel Henry <lionel.hry@gmail.com>
2016-10-06 19:19:59 +0200

    Simpler computation of extents
    For fallback: client can check if metrics are zero and then request
    fallback

--- src/CairoContext.cpp
+++ src/CairoContext.cpp
@@ -14,18 +14,15 @@ using namespace Rcpp;
 struct CairoContext::CairoContext_ {
   cairo_surface_t* surface;
   cairo_t* context;
-  bool currently_freetype; // FIXME: needed?
   FT_Library library;
   std::vector<FT_Face> ft_fonts;
   std::map<std::string, cairo_font_face_t*> fonts;
-  FontMetric fallback;
 };
 
 CairoContext::CairoContext() {
   cairo_ = new CairoContext_();
   cairo_->surface = cairo_pdf_surface_create(NULL, 720, 720);
   cairo_->context = cairo_create(cairo_->surface);
-  cairo_->currently_freetype = 0;
 
   if (FT_Init_FreeType(&(cairo_->library)))
     stop("FreeType error : unable to initialize FreeType library object.");
@@ -47,15 +44,12 @@ CairoContext::~CairoContext() {
 
 void CairoContext::setFont(std::string fontname, double fontsize,
                            bool bold, bool italic, std::string fontfile) {
+  cairo_set_font_size(cairo_->context, fontsize);
 
   if (fontfile.size())
     setUserFont(fontname, fontsize, bold, italic, fontfile);
   else
     setSystemFont(fontname, fontsize, bold, italic);
-
-  cairo_set_font_size(cairo_->context, fontsize);
-  // set fallback
-  cairo_->fallback = this->getExtents("M");
 }
 
 void CairoContext::setUserFont(std::string& fontname, double fontsize,
@@ -68,7 +62,6 @@ void CairoContext::setUserFont(std::string& fontname, double fontsize,
   }
 
   cairo_set_font_face(cairo_->context, cairo_->fonts[fontfile]);
-  cairo_->currently_freetype = 1;
 }
 
 void CairoContext::setSystemFont(std::string& fontname, double fontsize,
@@ -77,68 +70,17 @@ void CairoContext::setSystemFont(std::string& fontname, double fontsize,
                          fontname.c_str(),
                          italic ? CAIRO_FONT_SLANT_ITALIC : CAIRO_FONT_SLANT_NORMAL,
                          bold ? CAIRO_FONT_WEIGHT_BOLD : CAIRO_FONT_WEIGHT_NORMAL);
-  cairo_->currently_freetype = 0;
 }
 
 FontMetric CairoContext::getExtents(std::string x) {
-  cairo_glyph_t* glyphs = NULL;
-  int glyph_count;
-  cairo_text_cluster_t* clusters = NULL;
-  int cluster_count;
-  cairo_text_cluster_flags_t clusterflags;
-
-  cairo_status_t status = cairo_scaled_font_text_to_glyphs(cairo_get_scaled_font(cairo_->context),
-                                                           0, 0, x.c_str(), x.size(),
-                                                           &glyphs, &glyph_count,
-                                                           &clusters, &cluster_count, &clusterflags);
-  double w_ = 0.0;
-  double h_ = 0.0;
-  double a_ = 0.0;
-  double d_ = 0.0;
-  double descent = 0;
-  int has_warned = 0;
-
-  if (status == CAIRO_STATUS_SUCCESS) {
-
-    // for each cluster
-    int glyph_index = 0;
-    for (int i = 0; i < cluster_count; i++) {
-      cairo_text_cluster_t* cluster = &clusters[i];
-
-      cairo_glyph_t* clusterglyphs = &glyphs[glyph_index];
-      if( clusterglyphs[0].index > 0 ){
-        cairo_text_extents_t extents;
-        cairo_scaled_font_glyph_extents(cairo_get_scaled_font(cairo_->context), clusterglyphs, cluster->num_glyphs, &extents);
-        descent = extents.height + extents.y_bearing;
-
-        w_ += extents.x_advance;
-        h_ = (extents.height > h_) ? extents.height : h_;
-        a_ = (extents.y_bearing < a_) ? extents.y_bearing : a_;
-        d_ = (descent > d_) ? descent : d_;
-      } else {
-        if (!has_warned) {
-          warning("Cannot compute text metrics for selected font: symbols not found");
-          has_warned = 1;
-        }
-        w_ += cairo_->fallback.width;
-        h_ = (cairo_->fallback.height > h_) ? cairo_->fallback.height : h_;
-        a_ = (-cairo_->fallback.ascent < a_) ? -cairo_->fallback.ascent : a_;
-        d_ = (cairo_->fallback.descent > d_) ? cairo_->fallback.descent : d_;
-      }
-
-      glyph_index += cluster->num_glyphs;
-    }
-  } else stop("Could not get table of glyphs");
-
+  cairo_text_extents_t te;
+  cairo_text_extents(cairo_->context, x.c_str(), &te);
 
   FontMetric fm;
-  fm.height = h_;
-  fm.width = w_;
-  fm.ascent = -a_;
-  fm.descent = d_ ;
-
-  cairo_glyph_free(glyphs);
-  cairo_text_cluster_free(clusters);
+  fm.height = te.height;
+  fm.width = te.x_advance;
+  fm.ascent = -te.y_bearing;
+  fm.descent = te.height + te.y_bearing;
 
   return fm;
 }
--- tests/testthat/test-str_metrics.R
+++ tests/testthat/test-str_metrics.R
@@ -2,20 +2,9 @@ context("font file metric info")
 library(fontquiver)
 
 test_that("a string has positive dimensions", {
-  file <- fontquiver::font_bitstream_vera(style = "bold")$file
+  file <- fontquiver::font("Bitstream Vera", "Sans", "Bold")$ttf
   value <- str_metrics(x = "Hello World", fontsize = 12, fontfile = file )
   expect_true( value["width"] > 0)
   expect_true( value["ascent"] > 0 )
   expect_false( value["descent"] < 0 )
 })
-
-test_that("fallback usage", {
-  file <- fontquiver::font_bitstream_vera(variant = "sans", style = "bold-oblique")$file
-  expect_warning(
-    value_1 <- str_metrics( x = "\u265E", fontsize = 12, fontfile = file )
-  )
-  value_2 <- str_metrics( x = "M", fontsize = 12, fontfile = file )
-  expect_equivalent(value_1, value_2)
-})
-
-
