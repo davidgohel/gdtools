56c2e349386bed725054eff13a427c2286658258
Lionel Henry <lionel.hry@gmail.com>
2016-10-14 17:26:43 +0100

    Add test for metrics of known fonts

--- tests/testthat/test-str_extents.R
+++ tests/testthat/test-str_extents.R
@@ -5,3 +5,10 @@ test_that("a string has positive dimensions", {
   expect_true(all( value > 0 ))
 })
 
+test_that("known fonts have correct metrics", {
+  extents <- function(ttf) as.vector(round(str_extents("foobar", fontfile = ttf), 4))
+  sans <- fontquiver::font("Liberation", "Sans", "Regular")
+  mono_bi <- fontquiver::font("Bitstream Vera", "Sans Mono", "Bold Oblique")
+  expect_equal(extents(sans$ttf), c(34.0254, 8.8281))
+  expect_equal(extents(mono_bi$ttf), c(43.3477, 9.2969))
+})
