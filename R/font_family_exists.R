970aa59455233fc000ed303d310bb1e5f4d1ba04
dgohel <david.gohel@ardata.fr>
2016-10-29 12:01:45 +0200

    prepare for cran
    marked an example with `donttest`

--- R/font_family_exists.R
+++ R/font_family_exists.R
@@ -5,9 +5,11 @@
 #' @return A logical value
 #' @param font_family font family name (case sensitive)
 #' @examples
+#' \dontrun{
 #' font_family_exists("sans")
 #' font_family_exists("Arial")
 #' font_family_exists("Courier")
+#' }
 #' @export
 font_family_exists <- function( font_family = "sans" ){
   match_ <- match_family(font_family, FALSE, FALSE)
