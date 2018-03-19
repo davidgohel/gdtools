8ced0bbf9ef9c13da7f9c057814cd308ad179477
Jeroen Ooms <jeroenooms@gmail.com>
2018-03-19 03:27:25 +0100

    Windows: update cairo stack to 1.15.10

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,7 +1,7 @@
-# This version of libcairo works with Rtools
-if (!file.exists("../windows/cairo-1.14.2/cairo/cairo.h")) {
+VERSION <- commandArgs(TRUE)
+if(!file.exists(sprintf("../windows/cairo-%s/include/cairo/cairo.h", VERSION))){
   if(getRversion() < "3.3.0") setInternet2()
-  curl::curl_download("https://github.com/rwinlib/cairo/archive/v1.14.2.zip", "lib.zip")
+  download.file(sprintf("https://github.com/rwinlib/cairo/archive/v%s.zip", VERSION), "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
