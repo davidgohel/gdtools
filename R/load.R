b86397ebf722980295a0b74b67319fd7d5b79390
David Gohel <david.gohel@ardata.fr>
2019-05-06 16:50:36 +0200

    use magick fontconfig path instead of gdtools original one (not perfect as only when magick is available in the same library)

--- R/load.R
+++ R/load.R
@@ -4,6 +4,8 @@
 
     if( Sys.info()['sysname'] == "Windows" ){
       Sys.setenv(FONTCONFIG_PATH = file.path(lib, pkg, "fontconfig") )
+    } else if(file.exists(fontdir <- normalizePath(file.path(lib, "magick", "etc/fonts"), mustWork = FALSE))) {
+      Sys.setenv(FONTCONFIG_PATH=fontdir)
     } else if(file.exists("/opt/X11/lib/X11/fontconfig")){
         Sys.setenv(FONTCONFIG_PATH="/opt/X11/lib/X11/fontconfig")
     }
