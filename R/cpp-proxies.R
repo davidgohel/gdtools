1597138b56bc8b71ec537eed7ba1c6c211f55190
David Gohel <david.gohel@ardata.fr>
2019-09-03 15:12:31 +0200

    man

--- R/cpp-proxies.R
+++ R/cpp-proxies.R
@@ -9,13 +9,9 @@
 #' @param fontsize Font size
 #' @param fontfile Font file
 #' @examples
-#' \donttest{
-#' # The first run can be slow when font caches are missing
-#' # as font files are then being scanned to build those font caches.
 #' str_extents(letters)
 #' str_extents("Hello World!", bold = TRUE, italic = FALSE,
 #'   fontname = "sans", fontsize = 12)
-#' }
 #' @export
 str_extents <- function(x, fontname = "sans", fontsize = 12, bold = FALSE, italic = FALSE, fontfile = "") {
   str_extents_(x = x, fontname = fontname, fontsize = fontsize, bold = bold, italic = italic, fontfile = fontfile)
--- R/fontconfig.R
+++ R/fontconfig.R
@@ -3,7 +3,9 @@
 #' \code{match_family()} returns the best font family match.
 #'
 #' @param font family or face to match.
-#' @param bold,italic,debug deprecated
+#' @param bold Wheter to match a font featuring a \code{bold} face.
+#' @param italic Wheter to match a font featuring an \code{italic} face.
+#' @param debug deprecated
 #' @export
 #' @examples
 #' match_family("sans")
@@ -11,27 +13,17 @@
 #' @importFrom systemfonts match_font
 match_family <- function(font = "sans", bold = TRUE, italic = TRUE, debug = NULL) {
   # keep locale intact
-  font <- match_font(font)
+  font <- match_font(font, bold = bold, italic = italic)
   gsub( "\\.[A-Za-z]{3,}$", "", basename(font$path) )
 }
 
 
 #' Set and unset a minimalistic Fontconfig configuration
 #'
+#' @note
+#' Fontconfig is not used anymore and these functions will be deprecated
+#' in the next release.
 #' @export
-#' @examples
-#' if( require("fontquiver") ){
-#' on_appveyor <- function() {
-#'   identical(Sys.getenv("APPVEYOR"), "True")
-#' }
-#'
-#' # Use minimal fonts.conf to speed up fc-cache
-#' if (on_appveyor()) {
-#'   set_dummy_conf()
-#' }
-#'
-#' unset_dummy_conf()
-#' }
 set_dummy_conf <- function() {
 }
 
@@ -44,6 +36,9 @@ unset_dummy_conf <- function() {
 #' @title reload Fontconfig configuration
 #' @description This function can be used to make fontconfig
 #' reload font configuration files.
+#' @note
+#' Fontconfig is not used anymore and that function will be deprecated
+#' in the next release.
 #' @author Paul Murrell
 fontconfig_reinit <- function() {
 }
