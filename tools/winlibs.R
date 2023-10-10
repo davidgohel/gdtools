28cefbc0ca181ce9c34e411f2d712039d273fed0
Jeroen Ooms <jeroenooms@gmail.com>
2023-10-10 08:02:41 -0700

    Windows: update to libcairo 1.18

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,8 +1,18 @@
-VERSION <- commandArgs(TRUE)
-if(!file.exists(sprintf("../windows/cairo-%s/include/cairo/cairo.h", VERSION))){
-  if(getRversion() < "3.3.0") setInternet2()
-  download.file(sprintf("https://github.com/rwinlib/cairo/archive/v%s.zip", VERSION), "lib.zip", quiet = TRUE)
+if(!file.exists("../windows/cairo/include/cairo/cairo.h")){
+  unlink("../windows", recursive = TRUE)
+  url <- if(grepl("aarch", R.version$platform)){
+    "https://github.com/r-windows/bundles/releases/download/cairo-1.18.0/cairo-1.18.0-clang-aarch64.tar.xz"
+  } else if(grepl("clang", Sys.getenv('R_COMPILED_BY'))){
+    "https://github.com/r-windows/bundles/releases/download/cairo-1.18.0/cairo-1.18.0-clang-x86_64.tar.xz"
+  }  else if(getRversion() >= "4.2") {
+    "https://github.com/r-windows/bundles/releases/download/cairo-1.18.0/cairo-1.18.0-ucrt-x86_64.tar.xz"
+  } else {
+    "https://github.com/rwinlib/cairo/archive/v1.16.0.tar.gz"
+  }
+  download.file(url, basename(url), quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
-  unzip("lib.zip", exdir = "../windows")
-  unlink("lib.zip")
+  untar(basename(url), exdir = "../windows", tar = 'internal')
+  unlink(basename(url))
+  setwd("../windows")
+  file.rename(list.files(), 'cairo')
 }
